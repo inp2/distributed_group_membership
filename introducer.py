d441379ec776a1461677513d3b655baafc1b4321
Palmer <ipalmer2@fa16-cs425-g01-02.cs.illinois.edu>
2016-09-28 23:32:41 -0500

    updated introducer and node

--- introducer.py
+++ introducer.py
@@ -1,58 +1,118 @@
 #!/usr/bin/python
-import sys
 import logging
-import random
 import socket
+import sys
+import pickle
+import threading
 
-# Returns the membership list
-def create_memlist(filename):
-    mems = []
-    with open(filename, 'r') as my_file:
-        for line in my_file:
-            line = line.strip('\n')
-            memb = line
-            mems.append(memb)
-    logging.info("Initialized Membership List: " + str(mems))
-    return mems
 
-def conn_node(memblist):
-    fail_list = []
-    k = 1;
-    while True:
-        random.shuffle(memblist)
-        
-        for mem in memblist:
+class member_list:
+
+    def __init__(self):
+        self.lst = []
+        self.lock = threading.Lock()
+
+    def add(self, addr):
+        with self.lock:
+            self.lst.append(addr)
+
+    def remove(self, addr):
+        with self.lock:
+            self.lst.remove(addr)
+
+    def __str__(self):
+        rep =  '===== begin mlist =====\n'
+        for mem in self.lst:
+            rep += mem['host'] + ":" + str(mem['port']) + ":" + str(mem['time']
+            rep += '\n'
+        rep += '===== end mlist =====\n'
+        return rep
+
+
+class request_thread(threading.Thread):
+
+    def __init__(self, sock, addr, mlist):
+        super(request_thread, self).__init__()
+        self.sock = sock
+        self.addr = addr
+        self.mlist = mlist
+
+
+    def recv_msg(self):
+        sock = self.sock
+        while True:
+            msg = sock.recv(8129)
+            if msg.strip():
+                if msg.find('CMD_END') != -1:
+                    rcv_msg = pickle.loads(msg)
+                    self.sock.close()
+                    return rcv_msg
+
+        return None
+
+
+    def broadcast(self, msg):
+        logging.info("Broadcast Message")
+        for client in self.mlist.lst:
             try:
-                sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                mess = 'ping'
-                sock.settimeout(0.1)
-                sock.sendto(mess, (mem,10020))
-                data = ''
-                try:
-                    ret_buf, server_identity = sock.recvfrom(8192)
-                except socket.timeout:
-                    logging.info('ACK no received from node: ' + address)
-            except (socket.error,socket.gaierror) as err_msg:
-                logging.error('Socket Error: ' + str(err_msg))
-            sock.close()
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                sock.connect((client['host'], int(client['port'])))
+                send_msg = pickle.dumps(msg)
+                sock.send(send_msg + 'CMD_END')
+                logging.info("Node Command: " + msg['cmd'])
+                sock.close()
+            except (socket.error, socket.gaierror) as err_msg:
+                logging.exception(err_msg)
+                sock.close()
 
-def create_clist(memblist):
-    host_addr = [];
-    for name in memblist:
-        addr = socket.gethostbyname(name)
-        host_addr.append(addr)
-        logging.info(addr)
-    return host_addr
+    def obey(self, msg):
+        if msg['cmd'] == 'join' or msg['cmd'] == 'leave':
+            self.broadcast(msg)
+            mel = {'host':msg['host'], 'port':msg['port'], 'time':msg['time']}
+            if msg['cmd'] == 'join':
+                # mel = {'host':msg['host'], 'port':msg['port']}
+                self.mlist.add(mel)
+                print self.mlist
+                logging.info("Membership List Update: " + str(self.mlist))
+            else:
+                self.mlist.remove(mel)
+                print self.mlist
+                logging.info("Membership List Update: " + str(self.mlist))
+
+
+    def run(self):
+        msg = self.recv_msg()
+        self.obey(msg)
+
+
+def create_socket(host,port):
+    intro_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    intro_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    try:
+        intro_socket.bind((host,port))
+        logging.info("Introducer is Alive")
+    except (socket.error, socket.gaierror) as err_msg:
+        logging.exception(err_msg)
+        intro_socket.close()
+        sys.exit()
+    intro_socket.listen(10);
+    return intro_socket
+
+
+def accept_loop(intro_socket, memlist):
+    while True:
+        sock, addr = intro_socket.accept()
+        rt = request_thread(sock, addr, memlist)
+        rt.start()
+
 
 def main():
-    if len(sys.argv) < 2:
-        print 'python introducer.py'
-        sys.exit(1)
-    filename = sys.argv[1]
-    create_memlist(filename)
-    memblist = create_memlist(filename)
-    clientlist = create_clist(memblist)
-    conn_node(clientlist)
+    memlist = member_list()
+    host = ''
+    port = 10011
+    intro_socket = create_socket(host,port)
+    accept_loop(intro_socket, memlist)
+
 
 if __name__ == "__main__":
     logging.basicConfig(filename = "introducerdebug.log", level=logging.INFO,filemode = "w")
--- node.py
+++ node.py
@@ -1,23 +1,52 @@
 #!/usr/bin/python
 import logging
 import socket
+import sys
+import cPickle as pickle
+import threading
+import time
 
-def recv_ping():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    any_client = ('',10020)
-    while True:
-        try:
-            sock.bind(any_client)
-            buf = ''
-            while True:
-                buf, sender = sock.recvfrom(8192)
-                sock.sendto('ack', sender)
-        except (sock.error, socket.gaierror) as err_msg:
-            print err_msg
+def leave(host, port,tme):
+    logging.info("Connecting to Introducer")
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((host, int(port)))
+        # print time
+        msg = {'cmd': 'leave', 'host': socket.gethostbyname(socket.gethostname()), 'port': '10012', 'time':tme}
+        send_msg = pickle.dumps(msg)
+        sock.send(send_msg + 'CMD_END')
+        logging.info("Node Command: " + msg['cmd'])
+        sock.close()
+    except (socket.error, socket.gaierror) as err_msg:
+        logging.exception(err_msg)
+        sock.close()
+            
+def join(host, port, tme):
+    logging.info("Connecting to Introducer")
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((host, int(port)))
+        msg = {'cmd':'join', 'host': socket.gethostbyname(socket.gethostname()), 'port':'10012', 'time':tme}
+        send_msg = pickle.dumps(msg)
+        sock.send(send_msg + 'CMD_END')
+        logging.info("Node Command: " + msg['cmd'])
+        sock.close()
+    except (socket.error, socket.gaierror) as err_msg:
+        logging.exception(err_msg)
+        sock.close()
 
 def main():
-    recv_ping()
+    # host = '127.0.0.1'
+    host = 'fa16-cs425-g01-01'
+    port = '10011'
+    tme = time.time()
+    join_sig = raw_input('Enter "join":\n')
+    if join_sig == 'join':
+        join(host,port,tme)
+    # Check for voluntary leave
+    exit_sig = raw_input('Enter "exit" to voluntary leave:\n')
+    if exit_sig == 'exit':
+        leave(host,port,tme)
 
 if __name__ == "__main__":
     logging.basicConfig(filename="nodedebug.log", level=logging.INFO, filemode="w")
